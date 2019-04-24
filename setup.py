12ae6fe91aa271f7ff5995c321d7dc605195149c
sax <s.apostolico@gmail.com>
2019-04-24 13:12:57 +0200

    tox and travis

--- setup.py
+++ setup.py
@@ -28,35 +28,6 @@ def read(*files):
         content += codecs.open(os.path.join(HERE, f), 'r').read()
     return content
 
-
-class VerifyTagVersion(install):
-    """Verify that the git tag matches version"""
-
-    def run(self):
-        tag = os.getenv("CIRCLE_TAG")
-        if tag != VERSION:
-            info = "Git tag: {} does not match the version of this app: {}".format(
-                tag,
-                VERSION
-            )
-            sys.exit(info)
-
-
-class PyTest(TestCommand):
-    user_options = [('pytest-args=', 'a', "Arguments to pass to pytest")]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.pytest_args = ''
-
-    def run_tests(self):
-        import shlex
-        # import here, cause outside the eggs aren't loaded
-        import pytest
-        errno = pytest.main(shlex.split(self.pytest_args))
-        sys.exit(errno)
-
-
 setup(
     name=NAME,
     version=VERSION,
@@ -104,7 +75,4 @@ setup(
     package_dir={'': 'src'},
     packages=find_packages('src'),
     include_package_data=True,
-    cmdclass={"verify": VerifyTagVersion,
-              'test': PyTest,
-              }
 )
