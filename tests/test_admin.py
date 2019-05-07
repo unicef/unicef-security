4427067803e5a6b68ca57111a83a5062002cb5ba
denes csaba <csaba.denes@nordlogic.com>
2019-05-07 15:38:42 +0300

    cover admin reverse

--- tests/test_admin.py
+++ tests/test_admin.py
@@ -1,10 +1,18 @@
 from django.urls import reverse
 
+from unicef_security import admin
+from unicef_security.models import User
+
 
 def test_changelist(django_app, admin_user):
     url = reverse(f"admin:unicef_security_user_changelist")
     res = django_app.get(url, user=admin_user)
     assert res.status_code == 200
+
+
+def test_admin_reverse(django_app, admin_user):
+    url = reverse(f"admin:unicef_security_user_changelist")
+    assert admin.admin_reverse(User) == url
 
 
 def test_region_admin_sync(django_app, admin_user, vision_vcr):
