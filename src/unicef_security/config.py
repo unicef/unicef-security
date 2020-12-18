bce9b94e9cd27446e5cddb14065186a8909d9e09
Domenico <dom.dinicola@gmail.com>
2020-12-18 17:08:53 -0600

    config

--- src/unicef_security/config.py
+++ src/unicef_security/config.py
@@ -1,21 +1,28 @@
 import os
 
-AZURE_CLIENT_ID = os.environ.get('AZURE_CLIENT_ID', '')
-AZURE_CLIENT_SECRET = os.environ.get('AZURE_CLIENT_SECRET', '')
+from django.conf import settings
 
-INSIGHT_URL = os.environ.get('INSIGHT_URL', 'https://unibiapitest.azure-api.net/biapi/v1/')
-INSIGHT_SUB_KEY = os.environ.get('INSIGHT_SUB_KEY', 'insight_sub_key')
 
-GRAPH_CLIENT_ID = os.environ.get('GRAPH_CLIENT_ID', AZURE_CLIENT_ID)
-GRAPH_CLIENT_SECRET = os.environ.get('GRAPH_CLIENT_SECRET', AZURE_CLIENT_SECRET)
+def env_or_settings(name, default=None):
+    return os.environ.get(name, getattr(settings, name, default))
 
-AZURE_SSL = True
-AZURE_URL_EXPIRATION_SECS = 10800
-AZURE_ACCESS_POLICY_EXPIRY = 10800  # length of time before signature expires in seconds
-AZURE_ACCESS_POLICY_PERMISSION = 'r'
-AZURE_TOKEN_URL = 'https://login.microsoftonline.com/unicef.org/oauth2/token'
-AZURE_GRAPH_API_BASE_URL = 'https://graph.microsoft.com'
-AZURE_GRAPH_API_VERSION = 'v1.0'
-AZURE_GRAPH_API_PAGE_SIZE = 300
 
-UNICEF_EMAIL = os.environ.get('UNICEF_EMAIL', '@unicef.org')
+AZURE_CLIENT_ID = env_or_settings('AZURE_CLIENT_ID')
+AZURE_CLIENT_SECRET = env_or_settings('AZURE_CLIENT_SECRET')
+
+INSIGHT_URL = env_or_settings('INSIGHT_URL', 'https://unibiapitest.azure-api.net/biapi/v1/')
+INSIGHT_SUB_KEY = env_or_settings('INSIGHT_SUB_KEY', 'insight_sub_key')
+
+GRAPH_CLIENT_ID = env_or_settings('GRAPH_CLIENT_ID', AZURE_CLIENT_ID)
+GRAPH_CLIENT_SECRET = env_or_settings('GRAPH_CLIENT_SECRET', AZURE_CLIENT_SECRET)
+
+AZURE_SSL = env_or_settings('AZURE_SSL', True)
+AZURE_URL_EXPIRATION_SECS = int(env_or_settings('AZURE_URL_EXPIRATION_SECS', 10800))
+AZURE_ACCESS_POLICY_EXPIRY = int(env_or_settings('AZURE_ACCESS_POLICY_EXPIRY', 10800))
+AZURE_ACCESS_POLICY_PERMISSION = env_or_settings('AZURE_ACCESS_POLICY_PERMISSION', 'r')
+AZURE_TOKEN_URL = env_or_settings('AZURE_TOKEN_URL', 'https://login.microsoftonline.com/unicef.org/oauth2/token')
+AZURE_GRAPH_API_BASE_URL = env_or_settings('AZURE_GRAPH_API_BASE_URL', 'https://graph.microsoft.com')
+AZURE_GRAPH_API_VERSION = env_or_settings('AZURE_GRAPH_API_VERSION', 'v1.0')
+AZURE_GRAPH_API_PAGE_SIZE = int(env_or_settings('AZURE_GRAPH_API_PAGE_SIZE', 300))
+
+UNICEF_EMAIL = env_or_settings('UNICEF_EMAIL', '@unicef.org')
