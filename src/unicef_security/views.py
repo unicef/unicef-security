9ab5d23766bcca5044904e7b54fd9f842917ca16
Domenico <dom.dinicola@gmail.com>
2024-03-19 05:21:49 -0700

    fix admin creation (#38)

--- src/unicef_security/views.py
+++ src/unicef_security/views.py
@@ -10,10 +10,9 @@ class UnauthorizedView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context[
-            "unicef_user"
-        ] = self.request.user.is_authenticated and self.request.user.email.endswith(
-            UNICEF_EMAIL
+        context["unicef_user"] = (
+            self.request.user.is_authenticated
+            and self.request.user.email.endswith(UNICEF_EMAIL)
         )
         return context
 
