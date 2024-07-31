584d3d402fc3b087de1e2157ed5050d1caedbfca
Domenico DiNicola <dom.dinicola@gmail.com>
2024-07-31 13:00:31 +0200

    disable admin login

--- src/unicef_security/templatetags/security.py
+++ src/unicef_security/templatetags/security.py
@@ -8,3 +8,8 @@ register = Library()
 @register.simple_tag()
 def ad_backend():
     return reverse('social:begin', args=[getattr(settings, "SOCIAL_AUTH_BACKEND_NAME", "azuread-tenant-oauth2")])
+
+
+@register.simple_tag
+def settings_value(name):
+    return getattr(settings, name, "")
