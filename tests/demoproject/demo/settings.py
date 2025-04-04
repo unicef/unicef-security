8f09679d6d694d27e970d84ccfb9e22837caff34
Domenico <ddinicola@unicef.org>
2025-04-04 05:50:29 -0600

    dj5.2 (#43)

--- tests/demoproject/demo/settings.py
+++ tests/demoproject/demo/settings.py
@@ -58,6 +58,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "unicef_security.context_processors.current_state",
             ],
         },
     },
