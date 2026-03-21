cd2d365de8b45fc4df8bb0fd7404d08aa04b50b2
Domenico DiNicola <dom.dinicola@gmail.com>
2026-03-20 22:12:56 -0600

    fix

--- tests/demoproject/demo/models.py
+++ tests/demoproject/demo/models.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 
-from unicef_security.models import SecurityMixin
+from unicef_security.models import SecurityMixin, TimeStampedModel
 
 
-class User(SecurityMixin, AbstractUser):
+class User(TimeStampedModel, SecurityMixin, AbstractUser):
     class Meta(AbstractUser.Meta):
         app_label = "demo"
